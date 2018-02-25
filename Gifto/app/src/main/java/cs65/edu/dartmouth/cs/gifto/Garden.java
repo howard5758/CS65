6119021e3c1c432cc20353a6700fe3ddf295bcd7
norikil <1557441406@qq.com>
2018-02-24 23:32:08 -0500

    added view and object places to garden

--- Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/Garden.java
+++ Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/Garden.java
@@ -39,6 +39,13 @@ public class Garden extends Fragment {
         Button button_gifts = view.findViewById(R.id.button_gift_collections);
         Button button_animals = view.findViewById(R.id.button_pet_collections);
 
+        Button button_place1 = view.findViewById(R.id.button_place1);
+        Button button_place2 = view.findViewById(R.id.button_place2);
+        Button button_place3 = view.findViewById(R.id.button_place3);
+        Button button_place4 = view.findViewById(R.id.button_place4);
+        Button button_expand_left = view.findViewById(R.id.expand_left);
+        Button button_expand_right = view.findViewById(R.id.expand_right);
+
         // Food button
         button_food.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,11 +76,79 @@ public class Garden extends Fragment {
             }
         });
 
+        // Expand on the left side button, if no left side of garden, hint the users they can expand
+        // their garden
+        button_expand_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Currently no left side",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Expand on the right side button, if no right side of garden, hint the users they can expand
+        // their garden
+        button_expand_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Currently no right side",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button_place1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "choose food from your fridge",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button_place2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "choose from your storage",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button_place3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "choose from your storage",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button_place3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "choose from your toys",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        view = setBackground(view);
+
+        return view;
+
+    }
+
+    public View setBackground(View view){
         if (background == Globals.DEFAULT_BACKGROUND){
             ImageView bg = (ImageView) view.findViewById(R.id.bg);
             bg.setImageResource(R.drawable.plain_bg);
         }
-        return view;
+        if (place2 == Globals.DEFAULT_PLACE2){
+            ImageView p2 = (ImageView) view.findViewById(R.id.place2);
+            p2.setImageResource(R.drawable.tree);
+        }
+        if (place3 == Globals.DEFAULT_PLACE3){
+            ImageView p3 = (ImageView) view.findViewById(R.id.place3);
+            p3.setImageResource(R.drawable.pile_leaves);
+        }
 
+        return view;
     }
 }
+
