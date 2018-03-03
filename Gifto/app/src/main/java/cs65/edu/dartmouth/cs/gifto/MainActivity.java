8763abde8d4e85939d1ea90d3e4a0102878e948b
Jess <Jessica.E.Teipel.19@dartmouth.edu>
2018-03-03 15:25:54 -0500

    navbar works with map now

--- Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/MainActivity.java
+++ Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/MainActivity.java
@@ -22,7 +22,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
@@ -252,6 +251,7 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         }
+        navigationView.getMenu().getItem(0).setChecked(true);
     }
 
     @Override
--- Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/MapsActivity.java
+++ Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/MapsActivity.java
@@ -3,6 +3,8 @@ package cs65.edu.dartmouth.cs.gifto;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -14,10 +16,22 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -31,6 +45,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -41,7 +56,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, NavigationView.OnNavigationItemSelectedListener {
 
     private GoogleMap mMap;                     // google map
     private LocationManager lm;                 // finds last known location
@@ -76,6 +91,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         initLocationManager();  // get last known location
 
         mapFragment.setRetainInstance(true);
+
+        // toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Gifto");
+
+        //navigation view
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     /**
@@ -313,6 +342,82 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }//onActivityResult
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_garden) {
+            finish();
+        } else if (id == R.id.nav_map) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            for (UserInfo profile : user.getProviderData()) {
+                View headerView = navigationView.getHeaderView(0);
+                if (profile.getDisplayName() != null && !profile.getDisplayName().equals("")) {
+                    TextView tv = headerView.findViewById(R.id.nav_header_text);
+                    tv.setText(profile.getDisplayName());
+                }
+                if (profile.getPhotoUrl() != null) {
+                    ImageView navImage = headerView.findViewById(R.id.nav_image);
+                    navImage.setImageURI(profile.getPhotoUrl());
+                }
+            }
+        }
+        navigationView.getMenu().getItem(1).setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, UserProfile.class));
+            return true;
+        } else if (id == R.id.action_logout) {
+            Util.firebaseAuth.signOut();
+            Util.showActivity(this, LoginActivity.class);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+            finish();
+        }
+    }
 
     // save all the info if the screen is rotated
     protected void onSaveInstanceState(Bundle outState) {
