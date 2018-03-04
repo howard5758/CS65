c68b7ce70316ef6565e3b0207276271d5be08818
norikil <1557441406@qq.com>
2018-03-04 12:30:22 -0500

    typo

--- Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/Globals.java
+++ Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/Globals.java
@@ -40,7 +40,7 @@ public class Globals {
         ANIMAL_TO_TYPE.put("owl", 0);
         ANIMAL_TO_TYPE.put("sleeping owl", 0);
         ANIMAL_TO_TYPE.put("American Shorthair's head", 1);
-        ANIMAL_TO_TYPE.put("Sleeping American Shorthair", 1);
+        ANIMAL_TO_TYPE.put("sleeping American Shorthair", 1);
         ANIMAL_TO_TYPE.put("American Shorthair", 1);
         ANIMAL_TO_TYPE.put("sleeping Siamese Cat", 1);
         ANIMAL_TO_TYPE.put("Siamese Cat", 1);
@@ -48,7 +48,7 @@ public class Globals {
         ANIMAL_TO_TYPE.put("sleeping pink squirrel", 0);
         ANIMAL_TO_TYPE.put("pink squirrel", 0);
         ANIMAL_TO_TYPE.put("sleeping squirrel", 0);
-        ANIMAL_TO_TYPE.put("sleeping squirrel", 0);
+        ANIMAL_TO_TYPE.put("squirrel", 0);
         ANIMAL_TO_TYPE.put("unicorn with grass", 2);
         ANIMAL_TO_TYPE.put("sleeping unicorn", 2);
         ANIMAL_TO_TYPE.put("giggling unicorn", 2);
@@ -65,13 +65,13 @@ public class Globals {
          */
         ITEM_TO_TYPE.put("boxing glove",1);
         ITEM_TO_TYPE.put("butterfly teaser",1);
-        ITEM_TO_TYPE.put("white cursion",1);
-        ITEM_TO_TYPE.put("blue cloud cusion",1);
-        ITEM_TO_TYPE.put("orange cloud cusion",1);
-        ITEM_TO_TYPE.put("green cusion",1);
-        ITEM_TO_TYPE.put("pink cusion",1);
-        ITEM_TO_TYPE.put("purple cusion",1);
-        ITEM_TO_TYPE.put("yellow cusion",1);
+        ITEM_TO_TYPE.put("white cushion",1);
+        ITEM_TO_TYPE.put("blue cloud cushion",1);
+        ITEM_TO_TYPE.put("orange cloud cushion",1);
+        ITEM_TO_TYPE.put("green cushion",1);
+        ITEM_TO_TYPE.put("pink cushion",1);
+        ITEM_TO_TYPE.put("purple cushion",1);
+        ITEM_TO_TYPE.put("yellow cushion",1);
         ITEM_TO_TYPE.put("flower pot 1",1);
         ITEM_TO_TYPE.put("flower pot 2",1);
         ITEM_TO_TYPE.put("flower pot 3",1);
@@ -84,7 +84,7 @@ public class Globals {
         ITEM_TO_TYPE.put("brown pot",1);
         ITEM_TO_TYPE.put("green pot",1);
         ITEM_TO_TYPE.put("salad",0);
-        ITEM_TO_TYPE.put("yellow cusion",1);
+        ITEM_TO_TYPE.put("yellow cushion",1);
         ITEM_TO_TYPE.put("blue toy fish",1);
         ITEM_TO_TYPE.put("yellow toy fish",1);
         ITEM_TO_TYPE.put("toy reindeer",1);
@@ -93,6 +93,7 @@ public class Globals {
         ITEM_TO_TYPE.put("tennis ball",1);
         ITEM_TO_TYPE.put("trunk",2);
         ITEM_TO_TYPE.put("tuna",0);
+        ITEM_TO_TYPE.put("pile of leaves", 1);
 
         /** gifts: 0: small, can only carry messages
          * 1: medium, can carry messages and small items
@@ -116,4 +117,6 @@ public class Globals {
         GIFT_TO_TYPE.put("god food bag",1);
         GIFT_TO_TYPE.put("lucky bag",1);
     }
+
+
 }
--- Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/Util.java
+++ Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/Util.java
@@ -60,32 +60,180 @@ class Util {
     // returns the image that this gift/animal/toy/whatever is associated with
     public static int getImageIdFromName(String name){
         switch (name){
-            case "banana":
-                return R.drawable.banana;
-            case "tuna":
-                return R.drawable.tuna;
-            case "pool":
-                return R.drawable.pool;
-            case "tree":
-                return R.drawable.tree;
-            case "tennis ball":
-                return (R.drawable.tennis_ball);
             case "alligator":
                 return (R.drawable.alligator);
+            case "sleeping alligator":
+                return R.drawable.alligator_sleep;
+            case "blue bag":
+                return R.drawable.bag_blue;
+            case "green bag":
+                return R.drawable.bag_green;
+            case "pink bag":
+                return R.drawable.bag_pink;
+            case "watermelon bag":
+                return R.drawable.bag_watermelon;
+            case "red bag":
+                return R.drawable.bag_red;
+            case "banana":
+                return R.drawable.banana;
+            case "bat":
+                return R.drawable.bat;
+            case "sleeping bat":
+                return R.drawable.bat_sleep;
+            case "bg_magic":
+                return R.drawable.bg_magic;
+            case "bg_plain":
+                return R.drawable.bg_plain;
+            case "bg_savannah":
+                return R.drawable.bg_savannah;
+            case "blue box":
+                return R.drawable.box_blue;
+            case "green box":
+                return R.drawable.box_green;
+            case "purple box":
+                return R.drawable.box_purple;
+            case "red box":
+                return R.drawable.box_red;
+            case "yellow box":
+                return R.drawable.box_yellow;
+            case "boxing glove":
+                return R.drawable.boxing_glove;
+            case "butterfly teaser":
+                return R.drawable.butterfly_teaser;
             case "cat":
                 return (R.drawable.cat);
-            case "dog":
+            case "cat's head":
+                return (R.drawable.cat_head);
+            case "sleeping cat":
+                return (R.drawable.cat_sleep);
+            case "white cushion":
+                return (R.drawable.cursion_white);
+            case "blue cloud cushion":
+                return (R.drawable.cusion_cloud_blue);
+            case "orange cloud cushion":
+                return (R.drawable.cusion_cloud_orange);
+            case "green cushion":
+                return (R.drawable.cusion_green);
+            case "pink cushion":
+                return (R.drawable.cusion_pink);
+            case "purple cushion":
+                return (R.drawable.cusion_purple);
+            case "yellow cushion":
+                return (R.drawable.cusion_yellow);
+            case "Corgi":
+                return (R.drawable.dog);
+            case "Corgi's back":
+                return (R.drawable.dog_back);
+            case "sleeping Corgi":
+                return (R.drawable.dog_sleep);
+            case "Corgi's side":
                 return (R.drawable.dog_side);
+            case "flower pot 1":
+                return (R.drawable.flower_pot1);
+            case "flower pot 2":
+                return (R.drawable.flower_pot2);
+            case "flower pot 3":
+                return (R.drawable.flower_pot3);
+            case "food mat":
+                return (R.drawable.food_mat);
+            case "giant acorn":
+                return (R.drawable.giant_acorn);
+            case "gift_icon":
+                return (R.drawable.gift_icon);
+            case "grapes":
+                return (R.drawable.grapes);
+            case "hotdog":
+                return (R.drawable.hotdog);
             case "kangaroo":
                 return (R.drawable.kangaroo);
+            case "sleeping kangaroo":
+                return (R.drawable.kangaroo_sleep);
+            case "lucky bag":
+                return (R.drawable.lucky_bag);
+            case "magic egg":
+                return (R.drawable.magic_egg);
+            case "blue mitten":
+                return (R.drawable.mitten_blue);
+            case "yellow mitten":
+                return (R.drawable.mitten_yellow);
+            case "black mole bag":
+                return (R.drawable.mole_black);
+            case "blue mole bag":
+                return (R.drawable.mole_blue);
+            case "green mole bag":
+                return (R.drawable.mole_green);
+            case "purple mole bag":
+                return (R.drawable.mole_purple);
             case "monkey":
                 return (R.drawable.monkey);
+            case "monkey's head":
+                return (R.drawable.monkey_head);
+            case "sleeping monkey":
+                return (R.drawable.monkey_sleep);
+            case "monkey on tree with banana":
+                return (R.drawable.monkey_tree_banana);
+            case "monkey on tree":
+                return (R.drawable.monkey_tree);
             case "owl":
                 return (R.drawable.owl);
-            case "squirrel":
-                return (R.drawable.squirrel);
+            case "sleeping owl":
+                return (R.drawable.owl_sleep);
+            case "pile of leaves":
+                return (R.drawable.pile_leaves);
+            case "pool":
+                return (R.drawable.pool);
+
+            case "brown pot":
+                return (R.drawable.pot_brown);
+            case "green pot":
+                return (R.drawable.pot_green);
+            case "salad":
+                return (R.drawable.salad);
+            case "American Shorthair's head":
+                return (R.drawable.shorthair_head);
+            case "sleeping American Shorthair":
+                return (R.drawable.shorthair_sleep);
             case "American Shorthair":
                 return (R.drawable.shorthair);
+            case "Siamese Cat":
+                return (R.drawable.siamese);
+            case "sleeping Siamese Cat":
+                return (R.drawable.siamese_sleep);
+            case "Siamese Cat's head":
+                return (R.drawable.siamese_head);
+            case "squirrel":
+                return (R.drawable.squirrel);
+            case "sleeping squirrel":
+                return (R.drawable.squirrel_sleep);
+            case "sleeping pink squirrel":
+                return (R.drawable.squirrel_pink_sleep);
+            case "pink squirrel":
+                return (R.drawable.squirrel_pink);
+            case "tennis ball":
+                return (R.drawable.tennis_ball);
+            case "tiffany box":
+                return (R.drawable.tiffany_box);
+            case "blue toy fish":
+                return (R.drawable.toy_fish_blue);
+            case "yellow toy fish":
+                return (R.drawable.toy_fish_yellow);
+            case "toy reindeer":
+                return (R.drawable.toy_reindeer);
+            case "tuna":
+                return R.drawable.tuna;
+            case "tree":
+                return R.drawable.tree;
+            case "trunk":
+                return R.drawable.trunk;
+            case "unicorn with grass":
+                return R.drawable.unicorn_grass;
+            case "unicorn":
+                return R.drawable.unicorn;
+            case "sleeping unicorn":
+                return R.drawable.unicorn_sleep;
+            case "giggling unicorn":
+                return R.drawable.unicorn_smile;
+
             default:
                 return (R.drawable.tiffany_box);
         }
