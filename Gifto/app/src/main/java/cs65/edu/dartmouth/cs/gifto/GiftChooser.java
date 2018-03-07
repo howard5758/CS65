568d0435ce06e55964887c0726f1165bfd887657
Jess <Jessica.E.Teipel.19@dartmouth.edu>
2018-03-06 23:06:21 -0500

    minor ui changes. mostly worked on making giftchooser less ugly

--- Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/GiftChooser.java
+++ Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/GiftChooser.java
@@ -245,4 +245,9 @@ public class GiftChooser extends AppCompatActivity {
             finish();
         }
     }
+
+    public void onClick_edit(View view){
+        EditText editText = findViewById(R.id.editText_message);
+        editText.requestFocus();
+    }
 }
