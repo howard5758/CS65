fabdcebbb86bbc72c1aeefaad6801b22e982c91d
olivermct <olivermct@gmail.com>
2018-03-06 23:04:28 -0500

    made some files prettier

--- Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/Friend.java
+++ Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/Friend.java
@@ -3,10 +3,18 @@ package cs65.edu.dartmouth.cs.gifto;
 /**
  * Created by Oliver on 2/25/2018.
  *
- * Even though it's a very simple class, I think it will make adding to databases easier
+ * Class to store a single friend
+ *
+ * A friend is any other user. Users can add friends using the drop down menu in the garden
+ *      email: the email account that the friend signed up with
+ *      nickname: the nickname of the user, equal to the email if a nickname has not been set
+ *      firebaseId: the Firebase ID of the friend. The ID of the same user will be different in
+ *        different friend lists. This is okay, because globally the users are defined by their
+ *        email. The firebasFirebase ID is stored simply to make it easier to access a specific
+ *        friend for a specific user
  */
 
-public class Friend {
+class Friend {
     private String email;
     private String nickname;
     private String firebaseId;
@@ -18,33 +26,27 @@ public class Friend {
         firebaseId = "";
     }
 
-    // to initialize with name and nickname, if known
-    Friend(String email, String nickname) {
-        this.email = email;
-        this.nickname = nickname;
-    }
-
-    public String getNickname() {
+    String getNickname() {
         return nickname;
     }
 
-    public void setNickname(String nickname) {
+    void setNickname(String nickname) {
         this.nickname = nickname;
     }
 
-    public String getEmail() {
+    String getEmail() {
         return email;
     }
 
-    public void setEmail(String name) {
+    void setEmail(String name) {
         this.email = name;
     }
 
-    public String getFirebaseId() {
+    String getFirebaseId() {
         return firebaseId;
     }
 
-    public void setFirebaseId(String firebaseId) {
+    void setFirebaseId(String firebaseId) {
         this.firebaseId = firebaseId;
     }
 }
--- Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/GiftChooser.java
+++ Gifto/app/src/main/java/cs65/edu/dartmouth/cs/gifto/GiftChooser.java
@@ -23,6 +23,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Created by Jess on 2/28/2018.
+ *
+ *
+ * Activity to choose a gift to put on the map
+ * There are three spinners: the animal you want to use, the gift you want to send, and the friend
+ *   you want to send it to
+ * There is also an EditText to input a message if you would so like
+ *
+ * If you don't care who to send the gift to, then there is an option to send it to everyone
+ */
+
 public class GiftChooser extends AppCompatActivity {
 
     Spinner spinner_animal;
@@ -39,6 +51,7 @@ public class GiftChooser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gift_chooser);
 
+        // find all the necessary inputs
         helper = new MySQLiteHelper(this);
         spinner_animal = findViewById(R.id.spinner_animal);
         spinner_gift = findViewById(R.id.spinner_gift);
@@ -46,13 +59,11 @@ public class GiftChooser extends AppCompatActivity {
         editText_message = findViewById(R.id.editText_message);
         price_text = findViewById(R.id.text_price);
 
-        final Globals Globals = new Globals();
+        final List<String> spinnerArray =  new ArrayList<>();
+        final List<String> spinnerArray_friends =  new ArrayList<>();
 
-        final List<String> spinnerArray =  new ArrayList<String>();
-
-
-        final List<String> spinnerArray_friends =  new ArrayList<String>();
-        ArrayAdapter<String> adapter_friends = new ArrayAdapter<String>(
+        // set the items in the friend spinner
+        ArrayAdapter<String> adapter_friends = new ArrayAdapter<>(
                 getBaseContext(), android.R.layout.simple_spinner_item, spinnerArray_friends);
         adapter_friends.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerArray_friends.add("");
@@ -64,29 +75,39 @@ public class GiftChooser extends AppCompatActivity {
 
         // figure out which animals user can use to deliver gift
         // uses "animals" info from firebase
-        DatabaseReference ref = Util.databaseReference.child("users").child(Util.userID).child("animals");
+        DatabaseReference ref = Util.databaseReference.child("users")
+                .child(Util.userID).child("animals");
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot snapShot : dataSnapshot.getChildren()){
                     if(snapShot.child("present").getValue() != null) {
-                        if(snapShot.child("present").getValue(Integer.class) > -1) spinnerArray.add(snapShot.child("animalName").getValue(String.class));
+                        // add all the animals to the spinner
+                        if(Integer.parseInt(String.valueOf(
+                                snapShot.child("present").getValue())) > -1) {
+                            spinnerArray.add(snapShot.child("animalName").getValue(String.class));
+                        }
                     }
                 }
+
+                // if there are no animals, then don't let them send a gift
                 if(spinnerArray.size() == 0) {
                     Intent returnIntent = new Intent();
-                    Toast.makeText(getBaseContext(), "Need an animal to deliver gift", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getBaseContext(),
+                            "Need an animal to deliver gift", Toast.LENGTH_SHORT).show();
                     setResult(Activity.RESULT_CANCELED, returnIntent);
                     finish();
-                    return;
-                } else {
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                }
+
+                // otherwise, set up the spinner
+                else {
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(
                             getBaseContext(), android.R.layout.simple_spinner_item, spinnerArray);
                     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinner_animal.setAdapter(adapter);
 
                     // which items the user can send in the gift
-                    final List<String> spinnergift_array =  new ArrayList<String>();
+                    final List<String> spinnergift_array =  new ArrayList<>();
                     spinnergift_array.add("");
                     int animal_type;
                     if(Globals.ANIMAL_TO_TYPE.get(spinnerArray.get(0)) == null) animal_type = 0;
@@ -97,44 +118,62 @@ public class GiftChooser extends AppCompatActivity {
                         }
                     }
 
-                    ArrayAdapter<String> adapter_gift = new ArrayAdapter<String>(
-                            getBaseContext(), android.R.layout.simple_spinner_item, spinnergift_array);
-                    adapter_gift.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    ArrayAdapter<String> adapter_gift = new ArrayAdapter<>(getBaseContext(),
+                            android.R.layout.simple_spinner_item, spinnergift_array);
+                    adapter_gift.setDropDownViewResource(
+                            android.R.layout.simple_spinner_dropdown_item);
                     spinner_gift.setAdapter(adapter_gift);
                     spinner_gift.setEnabled(true);
 
-                    spinner_animal.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    spinner_animal.setOnItemSelectedListener(
+                            new AdapterView.OnItemSelectedListener() {
                         @Override
-                        public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                        public void onItemSelected(AdapterView<?> parentView,
+                                                   View selectedItemView, int position, long id) {
                             spinnergift_array.clear();
                             final int animal_type;
-                            if(Globals.ANIMAL_TO_TYPE.get(spinnerArray.get(position)) == null) animal_type = 0;
-                            else animal_type = Globals.ANIMAL_TO_TYPE.get(spinnerArray.get(position));
+                            if(Globals.ANIMAL_TO_TYPE.get(spinnerArray.get(position)) == null) {
+                                animal_type = 0;
+                            }
+                            else {
+                                animal_type=Globals.ANIMAL_TO_TYPE.get(spinnerArray.get(position));
+                            }
                             if(animal_type == 0) {
-                                Toast.makeText(getBaseContext(), "Need bigger animal to send item with message", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getBaseContext(),
+                                        "Need bigger animal to send item with message",
+                                        Toast.LENGTH_SHORT).show();
                                 spinnergift_array.add("");
                                 spinner_gift.setEnabled(false);
                                 ((BaseAdapter) spinner_gift.getAdapter()).notifyDataSetChanged();
                             } else {
                                 spinner_gift.setEnabled(true);
                                 spinnergift_array.add("");
-                                DatabaseReference ref2 = Util.databaseReference.child("users").child(Util.userID).child("items");
+                                DatabaseReference ref2 = Util.databaseReference.child("users")
+                                        .child(Util.userID).child("items");
                                 ref2.addListenerForSingleValueEvent(new ValueEventListener() {
                                     @Override
                                     public void onDataChange(DataSnapshot dataSnapshot) {
                                         for(DataSnapshot snapShot : dataSnapshot.getChildren()){
-                                            String gift_type = snapShot.child("itemName").getValue(String.class);
+                                            String gift_type = snapShot.child("itemName")
+                                                    .getValue(String.class);
                                             int item_amount;
-                                            if(snapShot.child("itemAmount").getValue() == null) item_amount = 0;
-                                            else item_amount = snapShot.child("itemAmount").getValue(Integer.class);
+                                            if(snapShot.child("itemAmount").getValue() == null) {
+                                                item_amount = 0;
+                                            }
+                                            else {
+                                                item_amount = Integer.parseInt(String.valueOf(
+                                                        snapShot.child("itemAmount").getValue()));
+                                            }
                                             if(Globals.ITEM_TO_TYPE.get(gift_type) != null){
                                                 int item_type = Globals.ITEM_TO_TYPE.get(gift_type);
-                                                if(item_type > 0 && item_type <= animal_type && item_amount > 0){
+                                                if(item_type > 0 && item_type <= animal_type
+                                                        && item_amount > 0){
                                                     spinnergift_array.add(gift_type);
                                                 }
                                             }
                                         }
-                                        ((BaseAdapter) spinner_gift.getAdapter()).notifyDataSetChanged();
+                                        ((BaseAdapter) spinner_gift.getAdapter())
+                                                .notifyDataSetChanged();
                                     }
 
                                     @Override
@@ -147,14 +186,14 @@ public class GiftChooser extends AppCompatActivity {
 
                         @Override
                         public void onNothingSelected(AdapterView<?> parentView) {
-                            // your code here
                         }
 
                     });
 
                     spinner_gift.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
                         @Override
-                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        public void onItemSelected(AdapterView<?> adapterView,
+                                                   View view, int i, long l) {
                             String gift_name = spinnergift_array.get(i);
                             int item_type;
                             if(Globals.ITEM_TO_TYPE.get(gift_name) != null){
@@ -162,11 +201,15 @@ public class GiftChooser extends AppCompatActivity {
                             } else {
                                 item_type = 0;
                             }
-                            ArrayList<String> size_strings = new ArrayList<String> (Arrays.asList("message", "small gift", "large gift"));
-                            ArrayList<Integer> cost_strings = new ArrayList<Integer> (Arrays.asList(1, 5, 15));
+                            ArrayList<String> size_strings = new ArrayList<>
+                                    (Arrays.asList("message", "small gift", "large gift"));
+                            ArrayList<Integer> cost_strings = new ArrayList<>
+                                    (Arrays.asList(1, 5, 15));
                             String size = size_strings.get(item_type);
                             cost = cost_strings.get(item_type);
-                            price_text.setText("Price to send " + size + " to a specific person: " + cost + " coins");
+                            String text = "Price to send " + size +
+                                    " to a specific person: " + cost + " coins";
+                            price_text.setText(text);
                         }
 
                         @Override
@@ -200,8 +243,7 @@ public class GiftChooser extends AppCompatActivity {
                 Garden.animal1_name.setText("");
                 Garden.item1_name.setText("");
                 Garden.loc1.setImageDrawable(null);
-                ArrayList<InventoryItem> food = new ArrayList<>();
-                food = helper.fetchAllInventoryItems();
+                ArrayList<InventoryItem> food = helper.fetchAllInventoryItems();
                 for(InventoryItem i: food){
                     if(i.getPresent() == 1){
                         i.setPresent(-1);
